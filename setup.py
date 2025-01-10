6a51054a60fc32cb1e2fa6eb5958200d6cad1ad2
Ubaidmalik9567 <Ubaidmalik9567@yahoo.com>
2025-01-10 22:36:18 +0500

    Change Description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Build End-to-End project with MLOPs that predicts food delivery time in minutes',
+    description='Build End-to-End project with MLOPs that predicts delivery time in minutes',
     author='Ubaid ur Rehman',
     license='BSD-3',
 )
